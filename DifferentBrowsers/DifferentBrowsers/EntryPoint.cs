cce3967fc491c7a3d2cbe24e675eb4da02f38290
jezzwilliams <jezzwilliams@gmail.com>
2019-01-12 15:39:07 +0000

    DataTime with Bob

---
+++ DifferentBrowsers/DifferentBrowsers/EntryPoint.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Opera;
+
+namespace DifferentBrowsers
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            IWebDriver driverChrome = new ChromeDriver();
+            IWebDriver driverIE = new InternetExplorerDriver();
+            IWebDriver driverFireFox = new FirefoxDriver();
+            //IWebDriver driverOpera = new OperaDriver();
+
+
+            //Run all browsers at the same time
+            string url = "http://google.com";
+            driverChrome.Navigate().GoToUrl(url);
+            driverIE.Navigate().GoToUrl(url);
+            driverFireFox.Navigate().GoToUrl(url);
+            //driverOpera.Navigate().GoToUrl(url);
+
+
+
+            //driverChrome.Quit();
+            //driverIE.Quit();
+            //driverOpera.Quit();
+
+        
+        }
+    }
+}
